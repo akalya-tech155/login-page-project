3539a90a0c9e5f71042903d3b850ff06ed5e14ef
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-09 18:04:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function login() {
+  let user = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+  let msg = document.getElementById("message");
+
+  if (user === "" || pass === "") {
+    msg.innerText = "Please fill all fields!";
+    msg.style.color = "red";
+  } else {
+    msg.innerText = "Login Successful!";
+    msg.style.color = "green";
+  }
+}
